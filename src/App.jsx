0b3ded61e7044a2aca75dadcb88699849b6d1eb0
Chris Williams <spider.mane.web@gmail.com>
2020-03-05 22:56:48 -0500

    added files for homepage

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomePage from './components/homepage/homepage';
+import './App.css';
+
+export default function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
